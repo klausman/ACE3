fcee3094644a01d4c2923b0d0c12de6c87c47b8d
bux <github@jonathandavid.de>
2021-11-14 07:56:12 +0100

    moving files

---
+++ docs/src/components/Navigation.vue
@@ -0,0 +1,18 @@
+<template>
+    <section w="full" bg="cool-gray-300 dark:light-50">
+        <h1>NAVIGATION</h1>
+        <img />
+        <nav>
+            <ul>
+                <li>
+                    <router-link to="/"> Home </router-link>
+                    <router-link to="/wiki"> Wiki </router-link>
+                    <router-link to="/news"> News </router-link>
+                    <router-link to="/maintainers"> Maintainers </router-link>
+                </li>
+            </ul>
+        </nav>
+    </section>
+</template>
+
+<script setup lang="ts"></script>
